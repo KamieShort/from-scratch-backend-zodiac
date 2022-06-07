fee865b2853e92471303fb11d90052c93d60c49d
Kamie Short <kamie.short@gmail.com>
2022-06-06 17:07:26 -0700

    set up tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,9 +3,26 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('from-scratch-backend-zodiac routes', () => {
+const { zodiac } = require('../data/zodiac');
+
+describe('zodiac routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('/zodiac should return an array of zodiac signs as objects', async () => {
+    const res = await request(app).get('/zodiac');
+    expect(res.body).toEqual(zodiac);
+  });
+
+  it('/zodiac/:id should return zodiac details', async () => {
+    const res = await request(app).get('/zodiac/1');
+    expect(res.body).toEqual({
+      id: '1',
+      name: 'aquarius',
+      dates: 'Jan 21 - Feb 19',
+      symbol: 'Water Bearer',
+    });
   });
 
   afterAll(() => {
